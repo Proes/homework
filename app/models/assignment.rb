01a00e160aabc7958d8635922705c33e5f8b1167
PolRod <pol@lumoslabs.com>
2016-09-25 13:46:22 +0200

    Annotate

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: assignments
+#
+#  id          :integer          not null, primary key
+#  question_id :integer
+#  user_id     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Assignment < ActiveRecord::Base
   validates :user, presence: true
   validates :homework, presence: true
--- app/models/homework.rb
+++ app/models/homework.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: homeworks
+#
+#  id         :integer          not null, primary key
+#  title      :string           not null
+#  question   :text             not null
+#  due_date   :datetime         not null
+#  user_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Homework < ActiveRecord::Base
   validates :title, presence: true
   validates :question, presence: true
